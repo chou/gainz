07cbe10612fe667c63f231481b0d287b3fae28f9
Jenny Chou <j.3h15u@gmail.com>
2014-07-04 20:08:03 -0700

    Refactoring error messages. Test cases for updating users with correct and wrong permissions.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,35 @@
 class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
-    user.assign_attributes(user_params)
-    if user.save
-      redirect_to dashboards_path
+    set_template_vars(user)
+
+    if current_user_authorized?
+      user.assign_attributes(user_params)
+      if user.save
+        set_template_vars(user)
+        redirect_to dashboards_path, status: :ok
+      else
+        add_generic_error!
+        render 'dashboards/index', status: :bad_request
+      end
     else
-      render_template 'dashboards/index'
+      add_generic_error!
+      render 'dashboards/index', status: :unauthorized
     end
   end
 
   private
+  def set_template_vars(user)
+    @id         = user.id
+    @height     = user.height
+    @weight     = user.weight
+    @birthdate  = user.birthdate
+    @lean_mass  = user.lean_mass
+    @activity_x = user.activity_x
+    @first_name = user.first_name
+    @last_name  = user.last_name
+  end
+
   def user_params
     params.require(:user).permit(User::PERMITTED_PARAMS)
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,25 +3,79 @@ require 'spec_helper'
 describe UsersController, type: :controller do
   describe '#update' do
     let(:permitted_params){
-                            { activity_x: '1.4',
+                            { activity_x: 1.4,
                               birthdate: '20110801',
                               email: 'max@gainz.com',
                               first_name: 'Max',
-                              height: '74',
+                              height: 74,
                               last_name: 'Gainz',
-                              lean_mass: '.9',
-                              weight: '190'
+                              lean_mass: 0.9,
+                              weight: 190
                             }
                           }
     let(:user){ create(User, permitted_params) }
 
-    context 'when the account edited belongs to the current user' do
-      it 'should work' do
-        # binding.pry
-        put :update, id: user.id, user: user.attributes
+    it 'should load the current template vars' do
+      expect(controller).to receive(:current_user).at_least(:once).and_return user
+      put :update, id: user.id, user: user.attributes.merge('first_name' => 'Moar')
 
-        expect(response).to redirect_to dashboards_path
-        expect(User.find(user.id).height).to eq 74
+      expect(assigns :id).to eq user.id
+      expect(assigns :height).to eq user.height
+      expect(assigns :weight).to eq user.weight
+      expect(assigns :birthdate).to eq user.birthdate
+      expect(assigns :lean_mass).to eq user.lean_mass
+      expect(assigns :activity_x).to eq user.activity_x
+      expect(assigns :first_name).to eq 'Moar'
+      expect(assigns :last_name).to eq user.last_name
+    end
+
+    context 'when the account edited belongs to the current user' do
+      before do
+        expect(controller).to receive(:current_user).at_least(:once).and_return user
+      end
+
+      context 'when the params are valid' do
+        it 'should succeed and redirect to the dashboards path' do
+          put :update, id: user.id, user: user.attributes
+
+          expect(response).to be_success
+          expect(User.find(user.id).height).to eq permitted_params[:height]
+        end
+      end
+
+      context 'when the params are invalid' do
+
+        let(:invalid_attrs){ user.attributes.merge 'activity_x' => 'FEELTHEPUMP' }
+
+        it 'should call #add_generic_error!' do
+          expect(controller).to receive :add_generic_error!
+          put :update, id: user.id, user: invalid_attrs
+          expect(response.status).to eq 400
+          expect(response).to render_template 'dashboards/index'
+        end
+      end
+    end
+
+    context 'when the account edited does not belong to the current user' do
+      let!(:victim) { create(User, id: 1765) }
+      let!(:malicious_user) { create(User) }
+      let(:troll_attrs) { victim.attributes.merge({ 'first_name' => 'No' }) }
+
+      before do
+        expect(controller).to receive(:current_user).at_least(:once).and_return malicious_user
+      end
+
+
+      it 'should forbid the update and re-render dashboards/index' do
+        put :update, id: victim.id, user: troll_attrs
+
+        expect(response.status).to eq 401
+        expect(response).to render_template 'dashboards/index'
+      end
+
+      it 'should call #add_generic_error!' do
+        expect(controller).to receive :add_generic_error!
+        put :update, id: victim.id, user: troll_attrs
       end
     end
   end
