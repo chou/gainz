a2fffa6bc30f9be2e007e9eeed49c9a38ecef6d1
Jenny Chou <j.3h15u@gmail.com>
2014-07-04 19:51:22 -0700

    Spec ApplicationController. Extract behavior for adding flash errors. Authorize current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  GENERIC_ERROR_MSG = 'Something went wrong; please try again.'.freeze
+
+  def add_generic_error!
+    flash[:error] = GENERIC_ERROR_MSG
+  end
+
   def after_sign_in_path_for(resource)
     dashboards_path
+  end
+
+  def current_user_authorized?
+    params[:user]['id'].to_i == current_user.id
   end
 end
---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe ApplicationController do
+  describe '#current_user_authorized?' do
+
+    context "when current user has different id from params[:user]['id']" do
+      let!(:victim) { create(User, id: 1765) }
+      let(:malicious_user) { create(User) }
+      let(:troll_attrs) { { user: victim.attributes.merge({ 'first_name' => 'No' }) } }
+
+      before do
+        expect(controller).to receive(:current_user).and_return malicious_user
+        expect(controller).to receive(:params).and_return troll_attrs
+      end
+
+      it 'should return false' do
+        expect(controller.current_user_authorized?).to be_falsy
+        #what's going on here? why does subject !== controller?
+      end
+    end
+
+    context "when current user has same id as params[:user]['id']" do
+      let(:user) { create(User) }
+      let(:new_attrs) { { user: user.attributes.merge({ 'activity_x' => 2 }) } }
+
+      before do
+        expect(controller).to receive(:current_user).and_return user
+        expect(controller).to receive(:params).and_return new_attrs
+      end
+
+      it 'should return true' do
+        expect(controller.current_user_authorized?).to be_truthy
+      end
+    end
+  end
+
+  describe '#add_generic_error!' do
+    it 'should add a particular string to flash[:error]' do
+      expect { controller.add_generic_error! }.
+        to change { flash[:error] }.
+          to ApplicationController::GENERIC_ERROR_MSG
+    end
+  end
+end
