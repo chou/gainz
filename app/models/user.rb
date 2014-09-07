3cfc7de0bbdfc032e9d42d636201972ee24a2038
Jenny Chou <j.3h15u@gmail.com>
2014-09-06 22:49:48 -0700

    Update validation syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,22 +2,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :email
-  validates_presence_of :birthdate,  on: :update
-  validates_presence_of :height,     on: :update
-  validates_presence_of :weight,     on: :update
-  validates_presence_of :activity_x, on: :update
-  validates_presence_of :lean_mass,  on: :update
-  validates_presence_of :first_name, on: :update
-  validates_presence_of :last_name,  on: :update
-
-  validates :height,      numericality: true, on: :update
-  validates :weight,      numericality: true, on: :update
-  validates :activity_x,  numericality: true, on: :update
-  validates :lean_mass,   numericality: true, on: :update
+  validates :email, presence: true, uniqueness: true
+  validates :birthdate, presence: true,  on: :update
+  validates :height, presence: true, numericality: true, on: :update
+  validates :weight, presence: true, numericality: true, on: :update
+  validates :activity_x, presence: true, numericality: true, on: :update
+  validates :lean_mass, presence: true, numericality: true, on: :update
+  validates :first_name, presence: true, on: :update
+  validates :last_name, presence: true,  on: :update
 
   validate :birthdate_is_date, on: :update
-  validates_uniqueness_of :email
 
   PERMITTED_PARAMS = [:email, :birthdate, :height, :weight, :activity_x,
                       :lean_mass, :first_name, :last_name, :goal].freeze
